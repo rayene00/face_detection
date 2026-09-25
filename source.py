1d638a213f8273927d56c32f94faa3955a65d9b8
rayene00 <rayene.mjth@gmail.com>
2026-09-25 20:20:48 +0200

    doc

--- source.py
+++ source.py
@@ -1,18 +1,21 @@
 import cv2
-from ultralytics import YOLO
+from ultralytics import YOLO #import for detection
 
-model = YOLO("yolov8n.pt")
-face_model = YOLO("yolov8m-face.pt")
+model = YOLO("yolov8n.pt").to("cuda") #model for image detection
+face_model = YOLO("yolov8m-face.pt").to("cuda")  #model for face detection
 
-photo = cv2.imread('image2.jpg')
-result = model(photo)
-face_result=face_model(photo)
+photo = cv2.imread('image2.jpg') #read the image2
 
-cat_person = result[0].plot()
+     
+result = model(photo) #detection of the image2 (what's inside)
+face_result=face_model(photo) #detection of the face(s) present on the photo
+    
+
+cat_person = result[0].plot() #
 cat_person_face = face_result[0].plot(img=cat_person)
 
 
-cv2.imshow("my window", cat_person_face)
-cv2.moveWindow("my window", 600, 5)
-cv2.waitKey(500000)
-cv2.destroyAllWindows()
+cv2.imshow("my window", cat_person_face) #display the window who contains the photo on the screen
+cv2.moveWindow("my window", 600, 5) #place where display appears
+cv2.waitKey(500000) #time of window in ms
+cv2.destroyAllWindows() #close the window when time finish
